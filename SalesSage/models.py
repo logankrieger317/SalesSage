6ffa3b025e26f524d6185e46458808b2900a7c47
Logan <logankrieger317@gmail.com>
2023-11-27 13:11:04 -0600

    Django Super User and GUI Working

--- SalesSage/models.py
+++ SalesSage/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+# tunr/models.py
+class User(models.Model):
+    name = models.CharField(max_length=100)
+    
+    
+    def __str__(self):
+        return self.name
