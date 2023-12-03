7c774cdd5313ad9bb39cce00f71753edb06a5069
Logan <logankrieger317@gmail.com>
2023-12-02 23:16:26 -0600

    Frustration Commit

--- server/db/index.cjs
+++
@@ -1,6 +0,0 @@
-const express = require('express');
-const server = express();
-
-const port = process.env.PORT || 3000;
-server.listen(port, () => console.log(`Server is running at http://localhost:${port}...`));
-
