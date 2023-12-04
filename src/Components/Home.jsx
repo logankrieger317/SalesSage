4a6780b7374227a33c4efb49513d823c28eab8c7
Logan <logankrieger317@gmail.com>
2023-12-04 15:35:00 -0600

    Styling on Home Page for Calendar+Chart

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,7 +1,8 @@
 import React, { useState } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
-import Calendar from "./Calendar";
+// import Calendar from "./Calendar";
+import Calendar from "react-calendar";
 import Charts from "./Charts";
 
 
@@ -16,15 +17,18 @@ const Home = () => {
     <>
       <Header />
       <p></p>
-      <div className="bg-green-100  ">
-        <h5 className="text-2lg text-center ">{getCurrentDate()}</h5>
+      <div className="bg-green-100  "> 
+        <h5 className="text-2lg text-center text-lg font-bold mt-2 mb-2">DTG: {getCurrentDate()}</h5>
         <div className="flex flex-col flex-wrap item-center justify-center w-100%  ">
-        {/* <Calendar/> */}
-
-  
+        <Calendar className="flex items-center flex-col justify-center p-4 mb-2 mt-2 font-bold shadow-2xl"/>
+        </div>
+        <div className=" font-bold text-lg" >
+        <h1 className=" flex items-center justify-center mb-4 mt-4 shadow-2xl ">Sales Vs. Labor</h1>
         <Charts  />
         </div>
       </div>
+      {/* </div> */}
+
       <Footer />
     </>
   );
